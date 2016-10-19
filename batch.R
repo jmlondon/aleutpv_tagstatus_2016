93bc472e41c44f656bf5c2c36fa94cd6c5408a58
Josh London <josh.london@noaa.gov>
2016-10-19 12:58:17 -0700

    COMMENT RSTUDIO_PANDOC ENV FOR TESTING

--- batch.R
+++ batch.R
@@ -1,6 +1,6 @@
-if (Sys.info()['sysname'] == "Linux") {
-  Sys.setenv(RSTUDIO_PANDOC = "//home//ec2-user//.local//bin//pandoc")
-}
+# if (Sys.info()['sysname'] == "Linux") {
+#   Sys.setenv(RSTUDIO_PANDOC = "/home/ec2-user/.local/bin/pandoc")
+# }
 
 if (Sys.info()['sysname'] == "Linux") {
   setwd('/home/ec2-user/aleutpv_tagstatus_2016')
